266958e864bff2665b919e83caa87a1dfb4c6550
Roberta Lima <rbnow12@gmail.com>
2022-07-03 23:28:27 -0300

    Classe produto passa atributos de nome, preço e id

--- api-spring/src/main/java/com/roberta/apispring/model/Product.java
+++ api-spring/src/main/java/com/roberta/apispring/model/Product.java
@@ -1,5 +1,26 @@
 package com.roberta.apispring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Product {
+
+    private @Id @GeneratedValue Long id;
+
+    @Column(name= "Nome", length = 200, nullable = false)
+    private String name;
+
+    @Column(name= "Preço", length = 200, nullable = false)
+    private double price;
     
 }
