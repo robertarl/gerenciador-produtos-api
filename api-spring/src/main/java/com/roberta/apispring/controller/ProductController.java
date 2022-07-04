3751d3242de8acf73d923240c53479eb81b5483f
Roberta Lima <rbnow12@gmail.com>
2022-07-03 23:41:27 -0300

    Metodo get implementado

--- api-spring/src/main/java/com/roberta/apispring/controller/ProductController.java
+++ api-spring/src/main/java/com/roberta/apispring/controller/ProductController.java
@@ -1,5 +1,29 @@
 package com.roberta.apispring.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.roberta.apispring.model.Product;
+import com.roberta.apispring.repository.ProductRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
     
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> listProd(){
+        return productRepository.findAll();
+    }
 }
