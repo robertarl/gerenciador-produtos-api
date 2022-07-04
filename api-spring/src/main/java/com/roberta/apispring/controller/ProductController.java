6cdbdb0a9e92ac73ca6708584a2171d3c2dc348e
Roberta Lima <rbnow12@gmail.com>
2022-07-03 23:43:59 -0300

    Metodo get por id implementado

--- api-spring/src/main/java/com/roberta/apispring/controller/ProductController.java
+++ api-spring/src/main/java/com/roberta/apispring/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,10 @@ public class ProductController {
     @ResponseStatus(code = HttpStatus.CREATED)
     Product create(@RequestBody Product prod) {
         return productRepository.save(prod);
+    }
+
+    @GetMapping({"/{id}"})
+    Product edit(@PathVariable Long id){
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
     }
 }
