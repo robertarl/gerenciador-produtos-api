36cbfcec08123528562f9790860c9242630a3cbd
Roberta Lima <rbnow12@gmail.com>
2022-07-03 23:26:33 -0300

    Interface de repositorio com herança

--- api-spring/src/main/java/com/roberta/apispring/repository/ProductRepository.java
+++ api-spring/src/main/java/com/roberta/apispring/repository/ProductRepository.java
@@ -1,5 +1,12 @@
 package com.roberta.apispring.repository;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.roberta.apispring.model.Product;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
     
 }
