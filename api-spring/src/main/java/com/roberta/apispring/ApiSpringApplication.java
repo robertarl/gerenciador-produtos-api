f39c954d95b658613f579095df2c6708227f199a
Roberta Lima <rbnow12@gmail.com>
2022-07-03 23:33:41 -0300

    Inicia dataBase

--- api-spring/src/main/java/com/roberta/apispring/ApiSpringApplication.java
+++ api-spring/src/main/java/com/roberta/apispring/ApiSpringApplication.java
@@ -1,13 +1,32 @@
 package com.roberta.apispring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.roberta.apispring.model.Product;
+import com.roberta.apispring.repository.ProductRepository;
 
 @SpringBootApplication
 public class ApiSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(ProductRepository productRepository){
+		return args -> {
+			productRepository.deleteAll();
+
+			Product prod = new Product();
+			prod.setName("SSD");
+			prod.setPrice(180.00);
+
+			productRepository.save(prod);
+			
+		}; 
 	}
 
 }
